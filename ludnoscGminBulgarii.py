d640c3e37f0414c8dd1cf8303b75ad1c848c523f
Tomasz Magulski <tomasz.magulski@gmail.com>
2011-11-14 13:39:23 +0100

    utworzono katalog na transliteracje, przeniesiono transliteracje bulgarska utworzono poczatek skryptu dla ludnosci bulgarii

---
+++ ludnoscGminBulgarii.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import bibliotek
+import sys, string
+from urllib2 import urlopen
+from wikipedia import *
+
+# ustawienia podstawowe
+mylang = 'pl'
+family = 'wikipedia'
+plwiki = getSite(mylang, family)
+
+# sciaganie danych zrodlowych
+areaSource = unicode(urlopen('http://www.nsi.bg/otrasal.php?otr=19&a1=376&a2=377&a3=378').read(), 'windows-1251', errors =  'ignore')
+print areaSource
+
+# ustalanie nazw gmin
+sourceMunicipalitiesList = {}
+
