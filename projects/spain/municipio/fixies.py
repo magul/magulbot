9b2db021ea223cdedd369dc19c5f2f4958cb77ae
Tomasz Magulski <tomasz.magulski@gmail.com>
2013-06-16 12:58:37 +0200

    retreiving names of spanish municipios from polish wiki

---
+++ projects/spain/municipio/fixies.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+provinces = [u'A Coruña', u'Araba', u'Albacete', u'Alicante', u'Almería', u'Asturia',
+             u'Ávila', u'Badajoz', u'Baleary', u'Barcelona', u'Burgos', u'Cáceres',
+             u'Castellón', u'Ciudad Real', u'Cuenca', u'Girona', u'Grenada', u'Guadalajara',
+             u'Guipúzcoa', u'Huelva', u'Huesca', u'Jaén', u'Kadyks', u'Kantabria', u'Kordoba',
+             u'La Rioja', u'León', u'Lleida', u'Lugo', u'Madryt', u'Malaga', u'Murcja',
+             u'Nawarra', u'Ourense', u'Palencia', u'Las Palmas', u'Pontevedra', u'Salamanka',
+             u'Santa Cruz de Tenerife', u'Saragossa', u'Segowia', u'Sewilla', u'Soria',
+             u'Tarragona', u'Teruel', u'Toledo', u'Valladolid', u'Vizcaya', u'Walencja',
+             u'Zamora']
+
+
---
+++ projects/spain/municipio/getplwikipagesfromtemplates.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# system imports
+from wikipedia import *
+import sqlite3 as lite
+
+# bot imports
+from fixies import *
+
+# configure plwiki
+pllang = 'pl'
+plfamily = 'wikipedia'
+plwiki = getSite(pllang, plfamily)
+
+# init municipio list
+plpage_plprovince = []
+
+# download templates of municipalies for every province
+for province in provinces:
+	page = Page(plwiki, u'Szablon:Prowincja ' + province)
+	pagetext = page.get()
+
+# find every plpage for municipio in templates
+	pagetext = pagetext[pagetext.find(u'spis1'):pagetext.rfind(u'</div>')]
+	muns = pagetext.split(u' • ')
+	for idx, item in enumerate(muns):
+		muns[idx] = item[item.find(u'[[')+2:item.find(u']]') if item.find(u'|')==-1 else item.find(u'|')]
+
+# check for redirections	
+	for idx, item in enumerate(muns):
+		page = Page(plwiki, item)
+		while page.isRedirectPage():
+			print item + u' => ' + page.getRedirectTarget().title()
+			item = page.getRedirectTarget().title()
+			page = Page(plwiki, item)
+		muns[idx] = item
+	for item in muns:
+		plpage_plprovince.append([item, province])
+
+# write to sqlite
+con = lite.connect('municipio.sqlite', isolation_level=None)
+cur = con.cursor()
+for result in plpage_plprovince:
+	cur.execute(u'INSERT INTO municipio (current_plpage, pl_province) VALUES("'+result[0]+'", "'+result[1]+'")')
+con.close()
