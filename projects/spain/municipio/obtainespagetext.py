a7e5183951df46e0a64e61a83ea33b4975c83fee
Tomasz Magulski <tomasz.magulski@gmail.com>
2013-06-21 17:33:13 +0200

    obtain wikidata for es.wiki and obtain es.wiki content

--- projects/spain/municipio/obtainespagetext.py
+++ projects/spain/municipio/obtainespagetext.py
@@ -17,19 +17,18 @@ eswiki = getSite(eslang, family)
 # open sqlite connection
 con = lite.connect('municipio.sqlite', isolation_level=None)
 cur = con.cursor()
-cur.execute(u'SELECT es_page FROM municipio where wikidata is null')
+cur.execute(u'SELECT es_page FROM municipio where es_page_text is null')
 data = cur.fetchall()
 
 for item in data:
 	es_page = item[0]
 	print es_page
-	es_page = Page(eswiki, es_page)
-	data_page = DataPage(es_page)
-	data_page.get()
-	data_page =  data_page.title()
-	es_page = es_page.title()
+	es_page_ = Page(eswiki, es_page)
+	text = es_page_.get()
+	text = text.replace("'", "''")
 
-	cur.execute(u'UPDATE municipio SET wikidata = "' + data_page + u'" where es_page = "' + es_page + u'"')
+
+	cur.execute(u"UPDATE municipio SET es_page_text = '" + text + u"' where es_page = '" + es_page.replace("'", "''") + u"'")
 
 # close sqlite connection
 con.close()
