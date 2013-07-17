c99a66b0a6f9cb42d4a68619c2b9bc6f1e1eb8e2
Tomasz Magulski <tomasz.magulski@gmail.com>
2013-07-17 08:46:47 +0200

    script that accualy put new item on wikidata as head of government of municipality of spain and make connection by claim

--- projects/spain/municipio/checkheadgov.py
+++ projects/spain/municipio/checkheadgov.py
@@ -49,8 +49,8 @@ eswiki = getSite('es','wikipedia')
 con = lite.connect('municipio.sqlite', isolation_level=None)
 cur = con.cursor()
 # Ficha de localidad de Espana
-cur.execute(u'SELECT es_page_text , es_page, gov from municipio where es_page_text like "%Ficha de localidad%"')
-data = cur.fetchall()
+#cur.execute(u'SELECT es_page_text , es_page, gov from municipio where es_page_text like "%Ficha de localidad%"')
+data = []#cur.fetchall()
 for item in data:
 	tempasdict = tem2dict(exttem(u'ficha de localidad', item[0]))
 	if u'alcalde' in tempasdict:
@@ -63,12 +63,54 @@ for item in data:
 		alcalde = alcalde[2:]
 		if alcalde.find(u']]')!= -1:
 			alcalde = alcalde[:alcalde.find(u']]')]
-		if Page(eswiki, alcalde).exists():
-			print item[2], '==>', alcalde
+		es_page = Page(eswiki, alcalde)
+		if es_page.exists():
+			while es_page.isRedirectPage():
+				es_page = es_page.getRedirectTarget()
+			data_page = DataPage(es_page)
+			data_page.get()
+			cur.execute(u'UPDATE municipio set gov_wd="'+data_page.title()+'" where es_page="'+item[1]+'"')
+#Ficha de entidad subn
+#cur.execute(u'SELECT es_page_text , es_page, gov from municipio where es_page_text like "%Ficha de entidad subn%"')
+data = []#cur.fetchall()
+for item in data:
+	tempasdict = tem2dict(exttem(u'ficha de entidad subn', item[0]))
+	alcalde = tempasdict[u'dirigentes_nombres']
+	if alcalde.find(u'[[') == 0:
+		alcalde = alcalde[2:]
+		if alcalde.find(u']]') != -1:
+			alcalde = alcalde[:alcalde.find(u']]')]
+		es_page = Page(eswiki, alcalde)
+		if es_page.exists():
+			while es_page.isRedirectPage():
+				es_page = es_page.getRedirectTarget()
+			data_page = DataPage(es_page)
+			data_page.get()
+			print es_page.title()
+			print data_page.title()
+			cur.execute(u'UPDATE municipio set gov_wd="'+data_page.title()+'" where es_page="'+item[1]+'"')
 
-
-
-cur.execute(u'SELECT es_page_text , es_page from municipio where es_page_text like "%Ficha de entidad subn%"')
+# compare es.wiki with wikidata
+repo = eswiki.data_repository()
+cur.execute(u'SELECT wikidata, gov_wd, gov from municipio')
 data = cur.fetchall()
+for item in data:
+	sql_mun = item[0]
+	if item[1]!=None:
+		sql_alc = int(item[1][1:])
+	else:
+		sql_alc = None
+	wd = DataPage(repo, item[0]).get()
+	wd_alc = None
+	claim_ct = 0
+	for claim in wd['claims']:
+		if claim['m'][1] == 6:
+			claim_ct += 1
+			wd_alc = claim['m'][3]['numeric-id']
+	if claim_ct > 1:
+		print sql_mun, '===> too much alcalde for municipio'
+		continue
+	if wd_alc != sql_alc and wd_alc != None:
+		print sql_mun, '===> alcalde not equal:', (sql_alc if sql_alc!=None else u'None'), '!=', wd_alc, '(in database:', item[2], ')'
 
 con.close()
---
+++ projects/spain/municipio/putheadgov.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# create item for head of goverment of municipio and property for item of municipio
+#
+
+# system imports
+import sqlite3 as lite
+from wikipedia import *
+
+con = lite.connect('municipio.sqlite', isolation_level=None)
+cur = con.cursor()
+
+cur.execute(u'SELECT wikidata, gov_wd, gov, spanish_name from municipio')
+data = cur.fetchall()
+for item in data:
+	mun_wd = int(item[0][1:])
+	if item[1] != None:
+		gov_wd = int(item[1][1:])
+	else:
+		gov_wd = None
+	gov = item[2]
+	sp_name = item[3]
+	
+	mun_page = DataPage(mun_wd)
+	mun_item = mun_page.get()
+
+	gov_ct = 0
+	for claim in mun_item['claims']:
+		if claim['m'][1] == 6:
+			gov_ct += 1
+	if gov_ct == 0 and gov!=None:
+		print mun_wd
+		gov_page = DataPage('es', gov)
+		gov_wd = int(gov_page.createitem(u'Creating item for alcalde of '+sp_name+', municipality of Spain', value={'labels': { 'es': {'language': 'es', 'value' : gov}, 'en': {'language': 'en', 'value' : gov}, 'pl': {'language': 'pl', 'value' : gov}}, 'sitelinks': {}})[2][u'entity'][u'id'][1:])
+		gov_page = DataPage(gov_wd)
+		gov_page.setitem(summary=u'Spanish description', items={'type': u'description', 'language': 'es', 'value': 'político de España'})
+		gov_page.setitem(summary=u'English description', items={'type': u'description', 'language': 'en', 'value': 'Spanish politician'})
+		gov_page.setitem(summary=u'Polish description', items={'type': u'description', 'language': 'pl', 'value': 'polityk hiszpański'})
+		mun_page.editclaim(6,gov_wd)
+		break
+
+
+con.close()
