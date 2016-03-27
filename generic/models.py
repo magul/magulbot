e264d285e502050d49ba5acab0789cdd6abcc4f1
Tomasz Magulski <tomasz@magullab.io>
2016-03-27 20:36:15 +0200

    reverting to abststract class

--- generic/models.py
+++ generic/models.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -10,26 +8,8 @@ class AbstractWikiDataEntity(models.Model):
     class Meta:
         abstract = True
 
-    wikidata_id_abs = models.CharField(max_length=15)
-    label_abs = models.CharField(max_length=150)
+    wikidata_id = models.CharField(max_length=15)
+    label = models.CharField(max_length=150)
 
     def __str__(self):
         return self.label_abs
-
-
-class WikiDataEntity(models.Model):
-
-    wikidata_id = models.CharField(max_length=15)
-    label = models.CharField(max_length=150)
-    reviewed_revision = models.CharField(max_length=15)
-    pending_revision = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.label
-
-
-class PendingReview(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    review_object = GenericForeignKey()
